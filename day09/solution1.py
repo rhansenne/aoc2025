7c87fd95aeaf7d8e8d70a5732943fa7702988558
rhansenne <rami.hansenne@digipolis.gent>
2025-12-09 19:53:29 +0100

    day09

--- day09/solution1.py
+++ day09/solution1.py
@@ -1,6 +1,3 @@
 import itertools
 tiles=[tuple(int(n) for n in l.split(','))  for l in open('input.txt', 'r').readlines()]
-mxarea=0
-for t1,t2 in itertools.combinations(tiles,2):
-    mxarea=max(mxarea,abs(t1[0]-t2[0]+1)*abs(t1[1]-t2[1]+1)) 
-print(mxarea)
+print(max([(abs(t1[0]-t2[0])+1)*(abs(t1[1]-t2[1])+1) for t1,t2 in itertools.combinations(tiles,2)]))
